23d9f746bfc6e21eeb49ae71f69a11086b3f5062
LinkWhale <filipbjed@gmail.com>
2024-03-22 14:29:57 +0100

    comments

--- maze3.py
+++ maze3.py
@@ -3,21 +3,23 @@ import random
 
 GridSize = 20
 
-
 x, y = 0, 0
 
-visited = [(x, y)]
-stack = [(x, y)]
+visited = [(x, y)] #contains every visited space in order they generate in
+stack = [(x, y)] #stack for the DPS algorithm
 
-directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+directions = [(1, 0), (-1, 0), (0, 1), (0, -1)] #directions a path can go
 
+#Generation loop
 while stack:
 
     x, y = stack[-1]
     random.shuffle(directions)
 
-    for dx, dy in directions:
-        nx, ny = x + dx, y + dy
+    for dx, dy in directions: #dx and dy stand for direction x and direction y
+        nx, ny = x + dx, y + dy #nx and ny stand for new x and new y
+
+        #contains the maze within borders and makes sure the space wasnt visited
         if nx >= 0 and nx <= GridSize and ny >= 0 and ny <= GridSize and (nx +dx, ny +dy) not in visited:
             visited.append((nx, ny))
             visited.append((nx + dx, ny + dy))
@@ -31,6 +33,7 @@ t.hideturtle()
 t.color("white")
 t.speed(0)
 
+#places a circle in every visited coordinate
 for x, y in visited:
     t.penup()
     t.setpos(x*10, y*10)
